aa195d7ffc0cfaf1574bc02ca083c6314ce347af
Erick Willyan <erickwillyan2105@gmail.com>
2025-02-04 20:53:55 -0300

    update: Header Reponsiva

--- repositories-project/src/components/Header/index.tsx
+++ repositories-project/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="absolute top-0 left-0 w-full p-4">
-      <h1 className="font-bold text-xl text-center">GitHunter</h1>
+      <h1 className="font-bold text-xl text-center sm:text-2xl">GitHunter</h1>
     </div>
   );
 }
